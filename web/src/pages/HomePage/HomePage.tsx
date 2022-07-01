64b9f32a1a4960a3741c403f01535a6cc7909ac5
nemanja.vasic.ext <nemanja.vasic.ext@adevinta.com>
2022-07-01 21:21:29 +0200

    Add Home page

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <div>
+        <h1>THE</h1>
+        <h1>GRAVEYARD</h1>
+        <h1>OF YOUR</h1>
+        <h1>FAILED</h1>
+        <h1>IDEAS</h1>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
