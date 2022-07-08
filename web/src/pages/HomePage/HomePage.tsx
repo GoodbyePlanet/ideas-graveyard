d14ace5ff3195198d0db49673797cf0dc2ed6132
nemanja.vasic.ext <nemanja.vasic.ext@adevinta.com>
2022-07-09 01:25:30 +0200

    Fix size of text on smaller height screen

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,15 @@
 import { MetaTags } from '@redwoodjs/web'
 
 import { About } from 'src/components/About'
-import { ScrollDownToGraveyard } from 'src/components/ScrollDownToGraveyard'
 
 import './HomePage.css'
 
 const HomePage = (): JSX.Element => {
   return (
-    <main>
+    <>
       <MetaTags title="Home" description="Home page" />
-      <section className="h-screen flex flex-col justify-center">
-        <div
-          className="app-title-font flex justify-center"
-        >
+      <div className="h-screen flex flex-col justify-center">
+        <div className="app-title-font flex justify-center min-h-0 overflow-auto">
           <div className="app-title-text font-black">
             <div>THE</div>
             <div>GRAVEYARD</div>
@@ -24,15 +21,9 @@ const HomePage = (): JSX.Element => {
         </div>
         <div className="flex flex-col items-center">
           <About />
-          <ScrollDownToGraveyard />
         </div>
-      </section>
-      <div className="text-center text-3xl">
-        <section id="graveyard" className="h-screen bg-orange-600">
-          <p>Graveyad page goes here...</p>
-        </section>
       </div>
-    </main>
+    </>
   )
 }
 
