12bafc20414ff7bae2a53cd288560bc541e5d2bb
nemanja.vasic.ext <nemanja.vasic.ext@adevinta.com>
2022-07-10 21:50:46 +0200

    WIP header and initial design for idea item

--- web/src/pages/GraveyardPage/GraveyardPage.tsx
+++ web/src/pages/GraveyardPage/GraveyardPage.tsx
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const GraveyardPage = () => {
@@ -6,14 +5,98 @@ const GraveyardPage = () => {
     <>
       <MetaTags title="Graveyard" description="Graveyard page" />
 
-      <h1>GraveyardPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/GraveyardPage/GraveyardPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>graveyard</code>, link to me with `
-        <Link to={routes.graveyard()}>Graveyard</Link>`
-      </p>
+      <header>
+        <div className="flex justify-end items-center h-14 border-b-2 border-gray-200">
+          <button className="text-base mr-4 hover:bg-black text-black hover:text-white py-2 px-4 border rounded">
+            Login
+          </button>
+        </div>
+        <div style={{ display: 'flex' }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '500px',
+              margin: '2rem',
+            }}
+          >
+            <img
+              style={{ width: '8rem', height: '8rem' }}
+              src="rip.png"
+              alt="as"
+            />
+            <p
+              style={{ marginTop: '1rem', fontSize: '1.5rem', fontWeight: 600 }}
+            >
+              Failed idea
+            </p>
+            <p
+              style={{
+                marginTop: '1rem',
+                textAlign: 'center',
+                fontSize: '1.2rem',
+              }}
+            >
+              This is my first failed idea. I started very entustiastic but
+              ended not even finishig it.
+            </p>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '500px',
+              margin: '2rem',
+            }}
+          >
+            <img
+              style={{ width: '8rem', height: '8rem' }}
+              src="rip.png"
+              alt="as"
+            />
+            <p
+              style={{ marginTop: '1rem', fontSize: '1.5rem', fontWeight: 600 }}
+            >
+              Failed idea
+            </p>
+            <p
+              style={{
+                marginTop: '1rem',
+                textAlign: 'center',
+                fontSize: '1.2rem',
+              }}
+            >
+              This is my first failed idea. I started very entustiastic but
+              ended not even finishig it.
+            </p>
+          </div>
+        </div>
+        <div
+          style={{
+            position: 'fixed',
+            right: 0,
+            border: '1px solid #512b2b',
+            marginRight: '-2rem',
+            borderRadius: '50%',
+            background: 'white',
+            cursor: "pointer",
+          }}
+        >
+          <img
+            style={{
+              height: '5rem',
+              width: '5rem',
+              paddingTop: '10px',
+              paddingBottom: '10px',
+              paddingRight: '15px',
+            }}
+            src="shovel.svg"
+            alt="Shovel"
+          />
+        </div>
+      </header>
     </>
   )
 }
